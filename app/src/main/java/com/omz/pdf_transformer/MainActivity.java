01b31db9e4eb6cfc711b92ccc156811b3e65b40d
omarnassif98 <33793027+omarnassif98@users.noreply.github.com>
2020-09-28 18:23:52 -0400

    Created the beginnings of a recents page. Added file picker functionality.

--- app/src/main/java/com/omz/pdf_transformer/MainActivity.java
+++ app/src/main/java/com/omz/pdf_transformer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.omz.pdf_transformer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,6 +10,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
@@ -19,38 +22,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent filePick = new Intent(Intent.ACTION_GET_CONTENT);
+                filePick.setType("application/pdf");
+                startActivityForResult(filePick, 2);
+                Log.d("fab", "pulling up file picker");
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        @Override
+        protected void onActivityResult(int req, int _res, Intent data) {
+            super.onActivityResult(req, _res, data);
+            Log.d("onRes", String.format("Called with req value %d and res value %d", req, _res));
+        if (req == 2 && _res == RESULT_OK) {
+            try {
+                Intent displayPDF = new Intent(this, PDFReader.class);
+                displayPDF.putExtra("pdfURI", data.getData());
+                Log.d("PDF loader", "valid pdf picked ");
+                startActivity(displayPDF);
+                //RENDER EACH PAGE AND PIPE IT FORWARD
+            } catch (Exception ex) {
+                Log.d("Activitycallback", "ERROR");
+                Log.d("Activitycallback", String.format("ERROR: %s", ex.getLocalizedMessage()));
+                Log.d("Activitycallback", String.format("ERROR: %s", Uri.parse(data.getDataString())));
+                Log.d("Activitycallback", String.format("ERROR: %s", data.getDataString()));
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            //
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
         }
-
-        return super.onOptionsItemSelected(item);
+        }
     }
-}
--- app/src/main/java/com/omz/pdf_transformer/PDFReader.java
+++ app/src/main/java/com/omz/pdf_transformer/PDFReader.java
@@ -2,14 +2,20 @@ package com.omz.pdf_transformer;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -19,14 +25,32 @@ import com.tom_roush.pdfbox.util.PDFBoxResourceLoader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class PDFReader extends AppCompatActivity {
+
+    private InputStream LoadPickedPDF() throws FileNotFoundException {
+        Intent intent = getIntent();
+        Uri pdfURI = intent.getParcelableExtra("pdfURI");
+        return getContentResolver().openInputStream(pdfURI);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pdf_view);
+
+        Intent intent = getIntent();
+        Uri pdfURI = intent.getParcelableExtra("pdfURI");
+        InputStream pfd = null;
+        try {
+            pfd = LoadPickedPDF();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         PDFBoxResourceLoader.init(getApplicationContext());
         final TextView documentTextView = findViewById(R.id.documentView);
         documentTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -39,11 +63,10 @@ public class PDFReader extends AppCompatActivity {
         String pdfName = pdfFile.substring(0, pdfFile.length() - 4);
         final SharedPreferences pref = getSharedPreferences("MyPref", 0);
         final SharedPreferences.Editor editor = pref.edit();
-
-
         AssetManager assetManager = getAssets();
         try {
-            final PDDocument document = PDDocument.load(assetManager.open(pdfFile));
+            final PDDocument document = PDDocument.load(pfd);
+            //PDDocument.load(pfd)
             int pageNumber = 1;
             editor.putInt("pageNumber", 0);
             pdfPageNumberView.setText(String.valueOf(pageNumber));
